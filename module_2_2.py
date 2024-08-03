eea9a2dbc521917d62e92871b461e51159aeb5bc
pati-patimat <patya.khaib@yandex.ru>
2024-08-03 19:04:43 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,6 +1,6 @@
-number_1 = input('Введите число: ')
-number_2 = input('Веедите число: ')
-number_3 = input('Веедите число: ')
+number_1 = int(input('Введите число: '))
+number_2 = int(input('Веедите число: '))
+number_3 = int(input('Веедите число: '))
 if number_1 == number_2 == number_3:
     print(3)
 elif number_1 == number_2 or number_2 == number_3 or number_3 == number_1:
